2e5be3652c620d6b803a9fcbdfdafcbaa39e6f35
leeechuan <aleechuan@gmail.com>
2024-01-08 01:12:59 +0800

    bug fix #03 08012024 - added mobile detection to fix touch not detected when on click of the navbar hamburger menu

--- client/src/scripts/homepage.js
+++ client/src/scripts/homepage.js
@@ -1,22 +1,17 @@
+import {isMobile} from 'react-device-detect'
+
 function NavbarListener() {
-    document.addEventListener("touchend", function(e) {
-        if (e.target.id === "navbar-hamburger"){
-          document.querySelector('.navbar-header').classList.remove('hidden');
-        //   document.getElementById('navbar-hamburger').style.visibility ='visible';
-        } else {
-          document.querySelector('.navbar-header').classList.add('hidden');
-        //   document.getElementById('navbar-hamburger').style.visibility ='hidden';
-        }
-    });
-    document.addEventListener("click", function(e) {
-        if (e.target.id === "navbar-hamburger"){
-          document.querySelector('.navbar-header').classList.remove('hidden');
-        //   document.getElementById('navbar-hamburger').style.visibility ='visible';
-        } else {
-          document.querySelector('.navbar-header').classList.add('hidden');
-        //   document.getElementById('navbar-hamburger').style.visibility ='hidden';
-        }
-    });
+    if(isMobile){
+        document.addEventListener("touchend", function(e) {
+            if (e.target.id === "navbar-hamburger"){
+              document.querySelector('.navbar-header').classList.remove('hidden');
+            //   document.getElementById('navbar-hamburger').style.visibility ='visible';
+            } else {
+              document.querySelector('.navbar-header').classList.add('hidden');
+            //   document.getElementById('navbar-hamburger').style.visibility ='hidden';
+            }
+        });
+    }
     
 }
 
