121be358190bc51f54a5ddde15a0e9f46a70c2ba
leeechuan <aleechuan@gmail.com>
2024-01-09 20:29:59 +0800

    hamburger menu bug fix 09012024

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -26,10 +26,18 @@ function Homepage(){
     useEffect(() => {
         const timeout = setTimeout(() => {
         setModalOpen(false);
-        }, 500000); // Close modal after 5 seconds
+        }, 30000); // Close modal after 30 seconds
       
         return () => clearTimeout(timeout);
     }, [])
+
+    
+    const [isMenuVisible, setMenuVisible] = useState(true);
+      
+    const toggleMenuVisibility = () => {
+        setMenuVisible(!isMenuVisible);
+    };
+
     
 
     const [goToLogin, setGoToLogin] = React.useState(false)
@@ -61,14 +69,16 @@ function Homepage(){
             </a>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button" onClick={()=> {setGoToLogin(true)}}  className="btn-primary text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Login</button>
-                <button onClick={()=> {alert("clicked")}} data-collapse-toggle="navbar-sticky" type="button" className="z-50 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false" id="navbar-hamburger">
+                <button onClick={toggleMenuVisibility} data-collapse-toggle="navbar-sticky" type="button" className="z-50 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
             </div>
-            <div className="navbar-header items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            {/* Conditonal rendering of toggle */}
+            {isMenuVisible ? null : (
+            <div className="navbar-header items-center justify-between w-full md:flex md:w-auto md:order-1">
                 <ul className="background-default md:gap-x-7 flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
                 <li>
                     <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">ABOUT US</a>
@@ -84,6 +94,7 @@ function Homepage(){
                 </li>
                 </ul>
             </div>
+            )}
             </div>
             </nav>
 
--- client/src/scripts/homepage.js
+++ client/src/scripts/homepage.js
@@ -2,15 +2,15 @@ import {isMobile} from 'react-device-detect'
 
 function NavbarListener() {
     if(isMobile){
-        document.addEventListener("touchend", function(e) {
-            if (e.target.id === "navbar-hamburger"){
-              document.querySelector('.navbar-header').classList.remove('hidden');
-            //   document.getElementById('navbar-hamburger').style.visibility ='visible';
-            } else {
-              document.querySelector('.navbar-header').classList.add('hidden');
-            //   document.getElementById('navbar-hamburger').style.visibility ='hidden';
-            }
-        });
+        // document.addEventListener("touchend", function(e) {
+        //     if (e.target.id === "navbar-hamburger"){
+        //       document.querySelector('.navbar-header').classList.remove('hidden');
+        //     //   document.getElementById('navbar-hamburger').style.visibility ='visible';
+        //     } else {
+        //       document.querySelector('.navbar-header').classList.add('hidden');
+        //     //   document.getElementById('navbar-hamburger').style.visibility ='hidden';
+        //     }
+        // });
     }
     
 }
