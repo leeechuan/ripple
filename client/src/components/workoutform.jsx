8583d0ac8d4902c5c030da462cc22c790b42125f
leeechuan <aleechuan@gmail.com>
2024-01-21 15:18:55 +0800

    big production issue is fixed/code clean up 21012024

--- client/src/components/workoutform.jsx
+++ client/src/components/workoutform.jsx
@@ -23,7 +23,7 @@ const WorkoutForm = () => {
 
         const workout = {calories, distance, duration}
 
-        const response = await fetch('http://localhost:3001/api/workouts',{
+        const response = await fetch('https://ripple-backend.vercel.app/api/workouts',{
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
