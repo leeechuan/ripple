e2fe607443f0caf994f9d26eb2fed0980f3647e6
leeechuan <aleechuan@gmail.com>
2024-01-21 13:30:07 +0800

    cors testing 21012024

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const userdetailRoutes = require('./routes/userdetail.js')
 
 // Express app
 const app = express()
-app.use(cors())
+// app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://theripplegym.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // Connect to vercel
