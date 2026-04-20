b8cb5973f3a93f2597f5a0c3dd208f7aa44a6240
leeechuan <aleechuan@gmail.com>
2026-04-20 23:26:09 +0800

    Hardcode API URL in Vite config to allow removal of .env

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_REACT_APP_API_URL': JSON.stringify('https://ripple-backend.vercel.app')
+  }
 })
